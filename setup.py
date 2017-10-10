5850a3f484b850ab5a1091f53105297bda019579
Fabio Caccamo <fabio.caccamo@gmail.com>
2017-10-10 11:15:36 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+import os
+
+exec(open('codicefiscale/version.py').read())
+
+github_url = 'https://github.com/fabiocaccamo'
+package_name = 'python-codicefiscale'
+package_path = os.path.abspath(os.path.dirname(__file__))
+long_description_file_path = os.path.join(package_path, 'README.rst')
+long_description = ''
+try:
+    with open(long_description_file_path) as f:
+        long_description = f.read()
+except IOError:
+    pass
+
+setup(
+    name=package_name,
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    include_package_data=True,
+    version=__version__,
+    description='python-codicefiscale is a tiny library for encode/decode Italian fiscal codes - codifica/decodifica del Codice Fiscale.',
+    long_description=long_description,
+    author='Fabio Caccamo',
+    author_email='fabio.caccamo@gmail.com',
+    url='%s/%s' % (github_url, package_name, ),
+    download_url='%s/%s/archive/%s.tar.gz' % (github_url, package_name, __version__, ),
+    keywords=['codicefiscale', 'codice', 'fiscale', 'cf', 'fiscal code', ],
+    install_requires=[
+        'python-dateutil >= 2.6.0, < 2.7.0',
+        'python-slugify >= 1.2.0, < 1.3.0',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    license='MIT',
+    test_suite='tests'
+)
