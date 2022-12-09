b9d39d2adbe5453a9e55e4ba1397e56ae5e69b81
Fabio Caccamo <fabio.caccamo@gmail.com>
2022-12-09 15:40:43 +0100

    Add failing test.

--- tests/tests.py
+++ tests/tests.py
@@ -263,6 +263,23 @@ class CodiceFiscaleTestCase(unittest.TestCase):
         with self.assertRaises(ValueError):
             codicefiscale.encode_cin("CCCFBA85D03")
 
+    def test_encode_issue_37(self):
+        # When encoding a CF for a person born in Vignola (MO) .encode uses code L884 (for Vignola (TN) between 1920-1929) instead of L885.
+        data = {
+            "surname": "Caccamo",
+            "name": "Fabio",
+            "sex": "M",
+            "birthdate": "03/04/1885",
+            "birthplace": "Vignola",
+        }
+        code = codicefiscale.encode(**data)
+        data = codicefiscale.decode(code)
+        birthplace = data["birthplace"]
+        self.assertTrue(birthplace is not None)
+        self.assertTrue(isinstance(birthplace, dict))
+        # print(birthplace)
+        self.assertEqual(birthplace["code"], "Vignola")
+
     def test_encode(self):
 
         data = [
